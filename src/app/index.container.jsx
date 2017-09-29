56840a80340dde9dcf0a56d713ac3f6dbfc7977a
cuongqle <lananh30011985>
2017-09-29 10:00:53 +0700

    add helmet meta tag for SEO

--- src/app/index.container.jsx
+++ src/app/index.container.jsx
@@ -1,9 +1,12 @@
 import React, {Component} from 'react';
+import Helmet from 'react-helmet';
+import config from 'config';
 
 export default class Index extends Component {
   render() {
     return (
         <div id="index">
+          <Helmet {...config.app.head}/>
           <div id="container">
             <div id="header"></div>
             <div className="children">
