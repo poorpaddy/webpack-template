14a14250fb48bb9ad132408953e1c50b493e0411
cuongqle <lananh30011985>
2017-09-30 12:23:43 +0700

    remove main container

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import { IndexRoute, Route } from 'react-router-dom';
-import MainContainer from "./app/main.container";
 import Header from "./app/header/header.component";
 import Footer from "./app/footer/footer.component";
+import HomePage from "./app/home/home.container";
+import FAQPage from "./app/faq/faq.container";
+import AboutPage from "./app/about/about.container";
 
 export default () => (
     <div>
       <Header />
-      <MainContainer/>
+      <div id="container">
+        <Route exact path="/" component={HomePage} />
+        <Route path="/faq" component={FAQPage} />
+        <Route path="/about" component={AboutPage} />
+      </div>
       <Footer/>
     </div>
 )
